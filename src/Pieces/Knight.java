15ac8a68b6d95d4b892d63c65d1b2c1510f81ada
Maury_Johnson <mauryljohnsonii@gmail.com>
2018-10-17 01:52:47 -0400

    B4IGO

--- src/Pieces/Knight.java
+++ src/Pieces/Knight.java
@@ -15,66 +15,118 @@ public class Knight<T,Q,R> extends Piece implements UpLeft<T,Q,R>,UpRight<T,Q,R>
 		super(Player);
 		// TODO Auto-generated constructor stub
 	}
-	
+	//////////
 	@Override
 	public R TryKnightRightUp() {
 		// TODO Auto-generated method stub
 		//Create Position that will be knight right right up
 		int[] P = new int[2];
 		
-		P[0] = CurrentPosition[0]+2;
-		P[1] = CurrentPosition[1]+1;
+		P[0] = CurrentPosition[0]+1;
+		P[1] = CurrentPosition[1]+2;
 		
-		ApplyMove(P);
-		return null;
+		int[] Ret = new int[1];
+		Ret[0] = ApplyMove(P);
+		
+		return (R)Ret;
+	}
+	///////////
+	@Override
+	public R TryKnightRightDown() {
+		// TODO Auto-generated method stub
+		int[] P = new int[2];
+		
+		P[0] = CurrentPosition[0]-1;
+		P[1] = CurrentPosition[1]+2;
+				
+		int[] Ret = new int[1];
+		Ret[0] = ApplyMove(P);
+
+		return (R)Ret;
 	}
 
 	@Override
-	public R TryKnightRightDown() {
+	public R TryKnightLeftUp() {
+		// TODO Auto-generated method stub
+		int[] P = new int[2];
+		
+		P[0] = CurrentPosition[0]+1;
+		P[1] = CurrentPosition[1]-2;
+		
+		int[] Ret = new int[1];
+		Ret[0] = ApplyMove(P);
+
+		return (R)Ret;
+	}
+
+	@Override
+	public R TryKnightLeftDown() {
+		// TODO Auto-generated method stub
+		int[] P = new int[2];
+		
+		P[0] = CurrentPosition[0]-1;
+		P[1] = CurrentPosition[1]-2;
+		
+		int[] Ret = new int[1];
+		Ret[0] = ApplyMove(P);
+
+		return (R)Ret;
+	}
+
+	@Override
+	public R TryDownRight() {
+		// TODO Auto-generated method stub
+		int[] P = new int[2];
+		
+		P[0] = CurrentPosition[0]-2;
+		P[1] = CurrentPosition[1]+1;
+		
+		int[] Ret = new int[1];
+		Ret[0] = ApplyMove(P);
+
+		return (R)Ret;
+	}
+
+	@Override
+	public R TryDownLeft() {
+		// TODO Auto-generated method stub
+		int[] P = new int[2];
+		
+		P[0] = CurrentPosition[0]-2;
+		P[1] = CurrentPosition[1]-1;
+		
+		int[] Ret = new int[1];
+		Ret[0] = ApplyMove(P);
+
+		return (R)Ret;
+	}
+
+	@Override
+	public R TryUpRight() {
 		// TODO Auto-generated method stub
 		int[] P = new int[2];
 		
 		P[0] = CurrentPosition[0]+2;
 		P[1] = CurrentPosition[1]+1;
 		
-		ApplyMove(P);
-		return null;
-	}
+		int[] Ret = new int[1];
+		Ret[0] = ApplyMove(P);
 
-	@Override
-	public R TryKnightLeftUp() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public R TryKnightLeftDown() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public R TryDownRight() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public R TryDownLeft() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public R TryUpRight() {
-		// TODO Auto-generated method stub
-		return null;
+		return (R)Ret;
 	}
 
 	@Override
 	public R TryUpLeft() {
 		// TODO Auto-generated method stub
-		return null;
+		int[] P = new int[2];
+		
+		P[0] = CurrentPosition[0]+2;
+		P[1] = CurrentPosition[1]-1;
+		
+		int[] Ret = new int[1];
+		Ret[0] = ApplyMove(P);
+
+		return (R)Ret;
 	}
 
 }
